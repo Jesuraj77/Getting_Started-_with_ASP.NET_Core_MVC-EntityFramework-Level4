ce2575694e25b322a49430c6c38580270fc710ce
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 15:58:56 +0530

    Code Added

---
+++ Level4.DataLayer/IDbConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level4.DataLayer
+{
+    public interface IDbConnectionFactory
+    {
+        Task<IDbConnection> CreateConnectionAsync();
+        Task SetupAsync();
+    }
+}
---
+++ Level4.DataLayer/SqLiteConnectionFactory.cs
@@ -0,0 +1,41 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level4.DataLayer
+{
+    public class SqLiteConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _dbLocation;
+
+        public SqLiteConnectionFactory(string dbLocation)
+        {
+            _dbLocation = dbLocation;
+        }
+
+
+        public async Task<IDbConnection> CreateConnectionAsync()
+        {
+            var connection = new SqliteConnection($"Data Source={_dbLocation}");
+            await connection.OpenAsync();
+            return connection;
+        }
+
+        public async Task SetupAsync()
+        {
+            //     if (!File.Exists(_dbLocation))
+            //  {
+            //       File.Create(_dbLocation).Close();
+            var connection = await CreateConnectionAsync();
+            await connection.ExecuteAsync("CREATE TABLE User (Id TEXT PRIMARY KEY NOT NULL, UserName TEXT NULL,Password TEXT NULL,ConfirmPassword TEXT NULL,Email TEXT NULL ,Photo TEXT NULL);");
+            //     }
+            //var connection = await CreateConnectionAsync();
+            //  await connection.ExecuteAsync("CREATE TABLE Products (Id TEXT PRIMARY KEY NOT NULL, Name TEXT NULL);");
+        }
+
+    }
+}
